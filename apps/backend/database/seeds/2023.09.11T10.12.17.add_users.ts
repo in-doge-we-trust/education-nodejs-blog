0e17cb56603e3bbf20b83fffd7c4d106a3cdf599
Bulat Murtazin <bulatf.murtazin@gmail.com>
2023-09-11 17:13:02 +0600

    Backend: code cleanup

--- apps/backend/database/seeds/2023.09.11T10.12.17.add_users.ts
+++ apps/backend/database/seeds/2023.09.11T10.12.17.add_users.ts
@@ -1,13 +1,17 @@
 import type { Seed } from '../seeder';
 
+const seedEntries = [
+  { nickname: 'bulat_m', email: 'bulat_m@gmail.com' },
+  { nickname: 'alina_a', email: 'alina_a@gmail.com' },
+  { nickname: 'eldar_m', email: 'eldar_m@gmail.com' },
+];
+
 export const up: Seed = async ({ context }) => {
-  await context.getQueryInterface().bulkInsert('users', [
-    { nickname: 'bulat_m', email: 'bulat_m@gmail.com' },
-    { nickname: 'alina_a', email: 'alina_a@gmail.com' },
-    { nickname: 'eldar_m', email: 'eldar_m@gmail.com' },
-  ]);
+  await context.getQueryInterface().bulkInsert('users', seedEntries);
 };
 
 export const down: Seed = async ({ context }) => {
-  await context.getQueryInterface().bulkDelete('users', {});
+  await context.getQueryInterface().bulkDelete('users', {
+    nickname: seedEntries.map(({ nickname }) => nickname),
+  });
 };
