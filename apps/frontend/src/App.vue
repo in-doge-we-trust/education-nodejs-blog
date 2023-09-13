b0966019bb7117eaf2a47ca562d693b212064e89
Bulat Murtazin <bulatf.murtazin@gmail.com>
2023-09-13 16:35:01 +0600

    Frontend: load and display posts

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -5,21 +5,26 @@
 </script>
 
 <template>
-  <main>
-    <h1>All posts</h1>
-
+  <header class="p-6 bg-cyan-700">
+    <h1 class="text-3xl text-neutral-100">All posts</h1>
+  </header>
+  <main class="p-6">
     <div v-if="arePostsLoading">
       <p>Loading posts...</p>
     </div>
-    <div v-else>
-      <article v-for="post in posts" :key="post.id">
-        <h2>
+    <div v-else class="flex flex-col gap-4">
+      <article
+        v-for="post in posts"
+        :key="post.id"
+        class="flex flex-col gap-2 p-4 border border-neutral-100 shadow-md"
+      >
+        <h2 class="text-2xl">
           {{ post.title }}
         </h2>
 
-        <div>
+        <p>
           {{ post.content }}
-        </div>
+        </p>
       </article>
     </div>
   </main>
